13d59efc567d5770910d1918ac008e521bee946f
Abdulmohsen <a.abanmy113@gmail.com>
2022-11-02 23:28:01 +0300

    Initial Push

---
+++ main.py
@@ -0,0 +1,42 @@
+from PIL import Image
+import math
+import numpy as np
+
+
+def pseudoRandom(seed):
+    # Under this line, put any complex mathmatical operations to the seed (an int) to make it as randomized as possible
+    #######
+
+    # Examples:
+    # n = (seed ** 2) % 45              # Bad, you can easily see patterns
+    # n = np.sin(seed * np.cos(seed))   # Good, no patterns can be easily detected.
+
+    # This should be a white screen indicating all output to be 1, the famous Trigonometry identity.
+    # n = np.sin(seed)**2 + np.cos(seed)**2
+
+    # Change this.
+    n = seed
+
+    #######
+    return n
+
+
+
+# Random Pattern Visualiser
+width = 500
+height = width
+
+pixels = np.arange(width*height, dtype=np.int64)
+pixels = pseudoRandom(pixels)
+
+# Processing
+pixels = np.abs(pixels)                             # Only positive values
+pixelScaled = pixels / pixels.max()                 # Scale down to be from 0 - 1
+pixels = np.round(pixelScaled * 255)                # Default, Allow grayscale.
+#pixelsBW = np.round(pixelScaled) * 255             # Alt, Either be white or black, No greyscaling.
+pixels = pixels.reshape((width, height))            # Reshape to see as a square image
+
+pixels = pixels                                     # pixelsGS = Grayscaled, pixelsBW = Black and White
+
+img = Image.fromarray(pixels)
+img.show()
